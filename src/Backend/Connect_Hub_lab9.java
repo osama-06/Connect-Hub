077293b47a90ce51d0cf475d0413fffc6ff53b29
osama-06 <osamahelmi06@gmail.com>
2024-12-04 21:11:40 +0200

    start project

---
+++ src/Backend/Connect_Hub_lab9.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Backend;
+
+/**
+ *
+ * @author User
+ */
+public class Connect_Hub_lab9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
