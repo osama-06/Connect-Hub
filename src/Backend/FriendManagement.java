fc4699993cc64e86349a5cc0f24e23daa70e811f
osama-06 <osamahelmi06@gmail.com>
2024-12-07 04:12:06 +0200

    change project name

--- src/Backend/FriendManagement.java
+++ src/Backend/FriendManagement.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Backend;
 
-/**
- *
- * @author Abdelrahman
- */
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
