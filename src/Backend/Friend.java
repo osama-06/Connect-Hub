7036aeef5505377912dd5803509aaf65ef5becae
osama-06 <osamahelmi06@gmail.com>
2024-12-13 19:51:00 +0200

    Update Friend.java

--- src/Backend/Friend.java
+++ src/Backend/Friend.java
@@ -1,36 +1,20 @@
 package Backend;
 
-public class Friend {
-    private String username;
-    private String profilePhoto; // Friend's profile photo path
+public class Friend extends User{
+    
 
     public Friend(String username, String profilePhoto) {
-        this.username = username;
-        this.profilePhoto = profilePhoto;
+       super.setUsername(username);
+        super.setProfilePhoto(profilePhoto);
+        
     }
 
-    // Getters and Setters
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getProfilePhoto() {
-        return profilePhoto;
-    }
-
-    public void setProfilePhoto(String profilePhoto) {
-        this.profilePhoto = profilePhoto;
-    }
-
+    
     @Override
     public String toString() {
         return "Friend{" +
-                "username='" + username + '\'' +
-                ", profilePhoto='" + profilePhoto + '\'' +
+                "username='" + super.getUsername() + '\'' +
+                ", profilePhoto='" + super.getProfilePhoto() + '\'' +
                 '}';
     }
 }
